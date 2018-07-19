6aca15fb221e7fe8444ca300f40799514d4d22c2
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-19 15:50:28 +0530

    Caesar Cipher assignment done!

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,23 +7,22 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            string output = string.Empty;
-
+            string output = "";
             foreach (char ch in text)
-                output += Cipher(ch, shiftKey);
-
+                output += CaesarCipherfunction(ch, shiftKey);
             return output;
             throw new NotImplementedException("You need to implement this function.");
         }
 
-        public static char Cipher(char ch, int key)
+        public static char CaesarCipherfunction(char ch, int key)
         {
             if (!char.IsLetter(ch))
             {
+
                 return ch;
             }
-            char d = char.IsUpper(ch) ? 'A' : 'a';
-            return (char)((((ch + key) - d) % 26) + d);
+            char CheckUpperorLower = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - CheckUpperorLower) % 26) + CheckUpperorLower);
         }
     }
 }
