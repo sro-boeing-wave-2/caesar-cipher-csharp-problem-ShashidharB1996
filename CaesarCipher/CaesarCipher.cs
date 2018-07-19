7a22fb7d4205bf62a0c6890b27fffa774f0431d3
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-19 15:22:31 +0530

    Initial solution - 10 cases passed

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,23 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            string output = string.Empty;
+
+            foreach (char ch in text)
+                output += Cipher(ch, shiftKey);
+
+            return output;
             throw new NotImplementedException("You need to implement this function.");
+        }
+
+        public static char Cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+                return ch;
+            }
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
         }
     }
 }
