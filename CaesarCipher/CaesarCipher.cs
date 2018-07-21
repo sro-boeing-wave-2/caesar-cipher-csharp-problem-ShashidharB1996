f58bf216ad3a1d03e8bc022ddcb3354b93f44aeb
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-21 10:31:52 +0530

    caesar cipher assignment done

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-
-namespace CaesarCipher
+﻿namespace CaesarCipher
 {
     public static class RotationalCipher
     {
@@ -9,20 +6,23 @@ namespace CaesarCipher
         {
             string output = "";
             foreach (char ch in text)
-                output += CaesarCipherfunction(ch, shiftKey);
+                if (char.IsLetter(ch))
+                {
+                    if (char.IsUpper(ch))
+                    {
+                        output += (char)((((ch + shiftKey) - 'A') % 26) + 'A');
+                    }
+                    else if (char.IsLower(ch))
+                    {
+                        output += (char)((((ch + shiftKey) - 'a') % 26) + 'a');
+                    }
+                }
+                 else
+                {
+                    output += ch;
+                }
+
             return output;
-            throw new NotImplementedException("You need to implement this function.");
-        }
-
-        public static char CaesarCipherfunction(char ch, int key)
-        {
-            if (!char.IsLetter(ch))
-            {
-
-                return ch;
-            }
-            char CheckUpperorLower = char.IsUpper(ch) ? 'A' : 'a';
-            return (char)((((ch + key) - CheckUpperorLower) % 26) + CheckUpperorLower);
         }
     }
 }
